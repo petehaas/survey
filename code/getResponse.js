2bab96f7d56678fdc750fa9c38306be2682c1200
phaas <phaas@verascape.com>
2020-01-17 08:23:20 -0600

    3rd commit

--- code/getResponse.js
+++ code/getResponse.js
@@ -2,7 +2,7 @@ module.exports.function = function getResponse (name, q1, q2) {
   return {
     name: name,
     q1: q1,
-    q2: q2,
+    q2: q2 ? q2 : false,
     // dynamic image from questions
     image: 'images/chart.png' // dummy chart example  
   }
